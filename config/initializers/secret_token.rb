be0ed82b70ccb0d09156dc788c970f2e6645d55d
sudohack <andreimpop@gmail.com>
2011-09-01 12:34:15 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Deathbymeetings::Application.config.secret_token = '86783fb88b4fa905186bcf6cbf0bc73ba45b0bb7b421f0016c21d513cbfd5be0cd9c470eeacea07f5311c24cf7b729def516c793ea67f6b4cd8112b2e9dfdb23'
